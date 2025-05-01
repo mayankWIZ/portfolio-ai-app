3b51cd6d8405c7565634fa3092ad27352f7314b8
Mayank Ranghadiya <mayankranghadiya54154@gmail.com>
2025-05-01 22:14:38 +0530

    fix: fe env

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -8,7 +8,7 @@ function Chat({ agentType, initialMessage, agentInitials, directQuestion }) {
   const messagesEndRef = useRef(null);
   const [processedQuestions, setProcessedQuestions] = useState([]);
 
-  const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+  const API_BASE_URL = import.meta.env.API_BASE_URL;
 
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
