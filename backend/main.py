5c0cc8bc5f527e4550a72a48313a83f861a90096
Mayank Ranghadiya <mayankranghadiya54154@gmail.com>
2025-05-01 21:19:35 +0530

    fix: with dockerfile

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,6 @@ class BaseAgent:
         self.description = description
 
         self.api_key = os.getenv("GROQ_API_KEY")
-        print("setting api key", self.api_key)
 
     def get_response(self, prompt):
 
--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -8,7 +8,7 @@ function Chat({ agentType, initialMessage, agentInitials, directQuestion }) {
   const messagesEndRef = useRef(null);
   const [processedQuestions, setProcessedQuestions] = useState([]);
 
-  const API_BASE_URL = "http://127.0.0.1:5001";
+  const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
